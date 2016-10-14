b266c9e2d631a802cf431d67c1471b33699f44aa
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-14 10:58:01 -0300

    fix: unit tests

--- test/01-revolution.js
+++ test/01-revolution.js
@@ -2,9 +2,9 @@ var
 	holidays = require('../index');
 
 
-console.log("25-04-2048 is "+(holidays.isHoliday(new Date(2048,4-1,25))?'holiday':'not a holiday'));
-if ( !holidays.isHoliday(new Date(2048,4-1,25)) ) {
-	console.log("Fail! 25-04-2048 should be a holiday");
+console.log("25-12-2048 is "+(holidays.isHoliday(new Date(2048,12-1,25))?'holiday':'not a holiday'));
+if ( !holidays.isHoliday(new Date(2048,12-1,25)) ) {
+	console.log("Fail! 25-12-2048 should be a holiday");
 	return process.exit(-1);
 }
 console.log("OK");
--- test/02-between.js
+++ test/02-between.js
@@ -3,8 +3,8 @@ var
 	thisYHolidays	= holidays.between(new Date(2015,0,1),new Date(2016,0,1));
 
 console.log("Holidays of 2015: ",thisYHolidays.length);
-if ( thisYHolidays.length != 9 ) {
-	console.log("Fail! 2015 has 9 holidays and we've got "+thisYHolidays.length);
+if ( thisYHolidays.length != 12 ) {
+	console.log("Fail! 2015 has 12 holidays and we've got "+thisYHolidays.length);
 	return process.exit(-1);
 }
 console.log("OK");
